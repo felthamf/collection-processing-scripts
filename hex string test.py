83a95ca08ea79de1b6556c7ba448e786f6716d89
Flora Feltham <flora.feltham@gmail.com>
2018-05-04 11:28:08 +1200

    Update hex string test.py

--- hex string test.py
+++ hex string test.py
@@ -2,24 +2,28 @@ import os
 import csv
 import binascii
 
+# put root directory here
+folder = r""
+signatures = []
 
-folder = r"Z:\manuscripts\Processing\COPIES  for processing\Flora_PRO_2002_Miria Simpson_A2002-379"
-
+# gets information about each directory and logs it to screen
 for root, subs, files in os.walk(folder):
 	print('Directory: %s' % root)
 	print ('Files: %s' % len(files))
-	s = set()
 	for doc in files:
 		print('%s' % doc)
 		docpath = os.path.join(root,doc)
 		with open(docpath,'r') as data:
 			bytes = data.read()
 			string = binascii.hexlify(bytes)
+			# take the first 150 bytes of a file. Can alter this
 			ministring = string[0:1200]
-			s.add(ministring)
-	print ('Hex signatures in Directory: %s' % len(s))
-
+			# creates a list of all the first 150 bytes of each file in the directory
+			signatures.append(ministring)
+print len(signatures)
+print signatures[1]
 
 # with open(r"Z:\manuscripts\Processing\COPIES  for processing\Miria_reporting\set_testing.txt", 'ab') as mydoc:
 # 	data.write(s)	
+
 
